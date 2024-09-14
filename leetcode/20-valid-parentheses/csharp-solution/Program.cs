9285a952bf6a6e83851ff78cdea4795a4b44ee99
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-09-14 18:39:56 +0200

    csharp solution to valid parentheses

---
+++ leetcode/20-valid-parentheses/csharp-solution/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace csharp_solution;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new();
+
+        foreach (char c in s)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    char compare = stack.Pop();
+
+                    if (c == ')' && compare == '(')
+                    {
+                        continue;
+                    }
+                    else if (c == ']' && compare == '[')
+                    {
+                        continue;
+                    }
+                    else if (c == '}' && compare == '{')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if (stack.Count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
